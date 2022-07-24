6cda501da1bebb9a867101fd191584b771225e64
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2022-07-24 11:22:37 +0200

    Minor changes to decision_tree training.

--- qc_analysis/medaka_qc_analysis.py
+++ qc_analysis/medaka_qc_analysis.py
@@ -18,14 +18,33 @@ import decision_tree.src.analysis as analyse
 
 def process_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input_file", help = "Results from medaka_bpm", type = str, required = True)
-    parser.add_argument("-o", "--out_dir",  help = "Directory to write analysis results", type = str, required = True)
+    parser.add_argument(
+        "input_file", 
+        type = str, 
+        help = "Training data file (csv)"
+    )
+
+    parser.add_argument(
+        "out_dir",  
+        type = str,
+        help = "Directory to write analysis results", 
+    )
+
+    parser.add_argument(
+        "-et",
+        "--error_threshold",
+        nargs = "?",
+        type = float,
+        default = 2.0,
+        help = "Error threshold (bpm) for decision tree"
+    )
+
     return parser.parse_args()
 
 def main():
     args = process_args()
     raw_data = analyse.pd.read_csv(args.input_file)
-    data, scale = analyse.process_data(raw_data, 20)
+    data, scale = analyse.process_data(raw_data, args.error_threshold)
     classifier, classifier_results = analyse.decision_tree(data)
     limits = analyse.get_thresholds(raw_data, analyse.QC_FEATURES, classifier)
     analyse.write_results(raw_data, data, classifier, classifier_results, limits, args.out_dir)
